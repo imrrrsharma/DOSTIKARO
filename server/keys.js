e3ba5ece9a943fd7e3bc959af816f20c6c7b7770
Rajat Sharma <110765339+imrrrsharma@users.noreply.github.com>
2022-10-28 09:48:00 +0530

    Add files via upload

---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URL:"mongodb+srv://imrrrsharma:iVakx0Rm4Zm9ZBp0@cluster0.a6nwsgn.mongodb.net/insta_clone?retryWrites=true&w=majority",
+    JWT_SECRET:"jhghfghfghf3ghf4hgf23"
+} 
